3b7c9f8a4f615941f045afb8b9c5bf91521f35d3
Matthias Blank <Matthias.Blank2@gmx.net>
2025-01-18 19:40:50 +0100

    Erster Commit

---
+++ src/main/java/com/example/MathUtils.java
@@ -0,0 +1,27 @@
+package com.example;
+
+/**
+ * Eine Utility-Klasse für mathematische Operationen.
+ */
+public class MathUtils {
+    
+    /**
+     * Addiert zwei Zahlen.
+     * @param a erste Zahl
+     * @param b zweite Zahl
+     * @return Summe der beiden Zahlen
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+    /**
+     * Multipliziert zwei Zahlen.
+     * @param a erste Zahl
+     * @param b zweite Zahl
+     * @return Produkt der beiden Zahlen
+     */
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+} 
---
+++ src/test/java/com/example/MathUtilsTest.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MathUtilsTest {
+    
+    @Test
+    public void testAdd() {
+        assertEquals("2 + 3 sollte 5 ergeben", 5, MathUtils.add(2, 3));
+        assertEquals("0 + 0 sollte 0 ergeben", 0, MathUtils.add(0, 0));
+        assertEquals("-1 + 1 sollte 0 ergeben", 0, MathUtils.add(-1, 1));
+    }
+    
+    @Test
+    public void testMultiply() {
+        assertEquals("2 * 3 sollte 6 ergeben", 6, MathUtils.multiply(2, 3));
+        assertEquals("0 * 5 sollte 0 ergeben", 0, MathUtils.multiply(0, 5));
+        assertEquals("-2 * 3 sollte -6 ergeben", -6, MathUtils.multiply(-2, 3));
+    }
+} 
